81b7b156dc69c4270df9710b122768316bcf2e19
Your Name <you@example.com>
2022-01-27 21:05:52 +0530

    users schema finalised

--- Models/users.js
+++ Models/users.js
@@ -19,13 +19,13 @@ const schema = new mongoose.Schema({
         },
         gender:{
             type:String,
-            enum:["Boys","Girls"],
-            required:true
+            enum:["Boys","Girls"]
         },
         roll:{
             type:String,
             default:"Not Given"
         },
+        address: String,
         year:{
             type:Number
         },
